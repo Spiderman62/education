127917b3186cac7d962dc95b1234045548ddc419
Spiderman62 <aztenderio7146@gmail.com>
2024-06-30 15:16:00 +0700

    Hieu dang fix cho nay

--- app/views/pages/clients/home.php
+++ app/views/pages/clients/home.php
@@ -2,4 +2,5 @@
 	<h1>This is a Bao</h1>
 	<h1>This is a Hoang</h1>
 	<h1>Đây là bao và bảo đang làm việc ở đây và mong muốn chờ thằng lâm hoàng xác nhận nhánh của mình vào nhánh gốc</h1>
+	<h1>Đây là hiếu và hiếu muốn vào làm việc cùng với mọi người</h1>
 </div>
