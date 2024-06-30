87a50f465615859837d53583fbe598aa5ba66863
Spiderman62 <aztenderio7146@gmail.com>
2024-06-30 15:28:10 +0700

    done

--- app/views/pages/clients/home.php
+++ app/views/pages/clients/home.php
@@ -1,6 +1,3 @@
 <div class="section_0">
-	<h1>This is a Bao</h1>
-	<h1>This is a Hoang</h1>
-	<h1>Đây là bao và bảo đang làm việc ở đây và mong muốn chờ thằng lâm hoàng xác nhận nhánh của mình vào nhánh gốc</h1>
-	<h1>Đây là hiếu và hiếu muốn vào làm việc cùng với mọi người</h1>
+	
 </div>
