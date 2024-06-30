acdfed14794c67a58773159534c87f30c2f6b29a
Spiderman62 <aztenderio7146@gmail.com>
2024-06-30 14:56:15 +0700

    Pending

--- app/views/pages/clients/home.php
+++ app/views/pages/clients/home.php
@@ -1,4 +1,5 @@
 <div class="section_0">
 	<h1>This is a Bao</h1>
 	<h1>This is a Hoang</h1>
+	<h1>Đây là bao và bảo đang làm việc ở đây và mong muốn chờ thằng lâm hoàng xác nhận nhánh của mình vào nhánh gốc</h1>
 </div>
