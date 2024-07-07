7a6cb2585277d8e74460345fc53f795b6b3980d8
Spiderman62 <aztenderio7146@gmail.com>
2024-07-07 21:38:08 +0700

    7/7/2024 9:38 PM

--- app/controllers/Quiz.php
+++ app/controllers/Quiz.php
@@ -2,18 +2,14 @@
 	class Quiz extends Controller{
 		private $data;
 		public function index(){
-			$this->data['aside_page'] = 'pages/clients/quiz';
 			$this->data['title'] = "Quiz";
-			$this->data['aside'] = 'layouts/aside';
-			$this->data['aside_css'] = 'clients/css/aside';
-			$this->data['aside_js'] = 'clients/javascript/aside';
+			$this->data['js'] = 'clients/javascript/quiz';
 			$this->data['css'] = 'clients/css/quiz';
+			$this->data['page'] = 'pages/clients/quiz';
 			$this->data['boxicons'] = true;
 			$this->data['jQuery'] = true;
 			$this->data['gsap'] = true;
 			$this->data['fonts'] = true;
-			// Lâm hoàng, theo tao nghĩ là cái quiz này nó thực cơ chế 1 trang nên là aside bảo luôn vào
-			// page luôn đi, khỏi bóc tách
 			$this->view('clients_layout',$this->data);
 			
 		}
--- app/views/clients_layout.php
+++ app/views/clients_layout.php
@@ -50,16 +50,6 @@
 	}
 	?>
 	<?php
-	if (!empty($aside)) {
-		$this->view($aside);
-	}
-	?>
-	<?php
-	if (!empty($aside_page)) {
-		$this->view($aside_page);
-	}
-	?>
-	<?php
 	if (!empty($page)) {
 		$this->view($page);
 	}
@@ -83,10 +73,6 @@
 	<?php if (!empty($js)) {
 	?>
 		<script src=<?php echo __ROOT__ . "public/" . $js . ".js?v=" . time(); ?>></script>
-	<?php } ?>
-	<?php if (!empty($aside_js)) {
-	?>
-		<script src=<?php echo __ROOT__ . "public/" . $aside_js . ".js?v=" . time(); ?>></script>
 	<?php } ?>
 	<?php if (!empty($header_js)) {
 	?>
