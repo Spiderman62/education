c86d24b3bcaf22e7806b1514d740a96ea4beedab
Spiderman62 <aztenderio7146@gmail.com>
2024-07-13 22:54:41 +0700

    13/7/2024 10:55 PM

--- app/views/pages/admin/admin.php
+++ app/views/pages/admin/admin.php
@@ -21,13 +21,13 @@
 				<li data-type="home">
 					<div class="icon">
 						<i class='bx bx-home-alt'></i>
-						
+
 					</div>
 					<span>Trang chủ</span>
 				</li>
 				<li data-type="user">
 					<div class="icon">
-					<i class="fa-solid fa-users"></i>
+						<i class="fa-solid fa-users"></i>
 					</div>
 					<span>Người dùng</span>
 				</li>
@@ -35,15 +35,8 @@
 					<div class="icon">
 						<i class='bx bx-category'></i>
 					</div>
-					<span>Danh mục</span>
+					<span>Danh mục khoá học</span>
 				</li>
-				<li data-type="subject">
-					<div class="icon">
-						<i class='bx bx-bar-chart-alt'></i>
-					</div>
-					<span>Môn học</span>
-				</li>
-				
 			</ul>
 		</div>
 		<div class="information">
@@ -57,23 +50,58 @@
 					<div class="switch"></div>
 				</div>
 			</div>
-			
+
 			<div class="profile">
 				<div class="user_image">
-					<img src=<?php echo !empty($_SESSION['info']['image'])? $_SESSION['info']['image']: __ROOT__ . "public/clients/images/anonymous.jpg" ?> alt="">
+					<img src=<?php echo !empty($_SESSION['info']['image']) ? $_SESSION['info']['image'] : __ROOT__ . "public/clients/images/anonymous.jpg" ?> alt="">
 				</div>
 				<div class="infor">
-					<span class="name"><?php echo $_SESSION['info']['username'];?></span>
+					<span class="name"><?php echo $_SESSION['info']['username']; ?></span>
 					<span class="major">Administrator</span>
 				</div>
 				<div class="logout">
 					<i class='bx bx-log-out'></i>
 				</div>
 			</div>
-					
+
 		</div>
 	</div>
 </aside>
-<section class="expand">
-	kasdlknds
+<section class="admin">
+	<div class="tabs">
+		<header>
+			<h1>THỐNG KÊ</h1>
+		</header>
+	</div>
+	<div class="tabs">
+		<header>
+			<h1>DANH SÁCH NGƯỜI DÙNG ( <span>19</span> )</h1>
+			<div class="stand-line"></div>
+			<h1>HIỂN THỊ ( <span>19</span> )</h1>
+			<div class="stand-line"></div>
+			<h1>THÙNG RÁC ( <span>0</span> )</h1>
+		</header>
+		<div class="wrapper-user">
+			<div class="switch">
+				<div class="role"><i class="fa-solid fa-user-group"></i>Sinh viên</div>
+				<div class="stand-line"></div>
+				<div class="role"><i class="fa-solid fa-user-graduate"></i>Giảng viên</div>
+				<div class="line"></div>
+			</div>
+			<main>
+				<div class="switch-active">
+					Đây là trang sinh viên
+				</div>
+				<div class="switch-active">
+					đây là trang giảng viên
+				</div>
+			</main>
+		</div>
+
+	</div>
+	<div class="tabs">
+		<header>
+			<h1>DANH SÁCH KHOÁ HỌC</h1>
+		</header>
+	</div>
 </section>
--- public/admin/javascript/admin.js
+++ public/admin/javascript/admin.js
@@ -1,19 +1,59 @@
-const aside = {
-	main() {
-		$('aside .information .mode-switch .wrapper_mode-switch').on('click', function () {
-			$('body').toggleClass('dark');
-			if($('body').hasClass('dark')){
-				$('aside .information .mode-switch .type').text('Light mode');
-			}else{
-				$('aside .information .mode-switch .type').text('Dark mode');
-			}
-		});
-		$('aside header .toggle').on('click',function(){
-			$('aside').toggleClass('close');
-		});
-		$('aside .menu .search-box .icon').on('click',function(){
-			$('aside').removeClass('close');
-		});
+$(function () {
+	const aside = {
+		main() {
+			$('aside .information .mode-switch .wrapper_mode-switch').on('click', function () {
+				$('body').toggleClass('dark');
+				if ($('body').hasClass('dark')) {
+					$('aside .information .mode-switch .type').text('Light mode');
+				} else {
+					$('aside .information .mode-switch .type').text('Dark mode');
+				}
+			});
+			$('aside header .toggle').on('click', function () {
+				$('aside').toggleClass('close');
+			});
+			$('aside .menu .search-box .icon').on('click', function () {
+				$('aside').removeClass('close');
+			});
+		}
 	}
-}
-aside.main();
+	aside.main();
+	const tabs = {
+		handleTabs() {
+			$('.admin .tabs').hide();
+			$('.admin .tabs').eq(1).show();
+			$('aside .menu .menu-tab ul li').eq(1).addClass('mark');
+			$('aside .menu .menu-tab ul li').on('click', function () {
+				const index = $(this).index();
+				$('.admin .tabs').fadeOut(0);
+				$('aside .menu .menu-tab ul li').removeClass('mark');
+				$('.admin .tabs').eq(index).fadeIn(300);
+				$('aside .menu .menu-tab ul li').eq(index).addClass('mark');
+			});
+		},
+		main() {
+			this.handleTabs();
+		}
+	}
+	tabs.main();
+	const switchRole = {
+		handleSwitchRole() {
+			const roleElementWidth = $('section .tabs .wrapper-user .switch .role').eq(0).width();
+			$('section .tabs .wrapper-user .switch .line').css('left', `0px`);
+			$('section .tabs .wrapper-user .switch .line').css('width', `${roleElementWidth}px`);
+			$('section .tabs main .switch-active').hide();
+			$('section .tabs main .switch-active').eq(0).show();
+			$('section .tabs .wrapper-user .switch .role').on('click', function () {
+				const index = $(this).closest('.wrapper-user').find('.role').index(this);
+				const position = $(this).position().left;
+				$('section .tabs .wrapper-user .switch .line').css('left', `${position}px`);
+				$('section .tabs main .switch-active').fadeOut();
+				$('section .tabs main .switch-active').eq(index).fadeIn();
+			});
+		},
+		main() {
+			this.handleSwitchRole();
+		}
+	};
+	switchRole.main();
+});
