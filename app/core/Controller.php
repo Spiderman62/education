f8a32b2a83119b3e3158b4da2eda9c39b1e0987f
Nguyễn Lâm Hoàng <140186756+Spiderman62@users.noreply.github.com>
2024-11-19 20:40:30 +0700

    Update Controller.php

--- app/core/Controller.php
+++ app/core/Controller.php
@@ -16,25 +16,25 @@ class Controller
 			require_once "./app/views/" . $view . ".php";
 		}
 	}
-	// public function sendEmail($title, $html,$email)
-	// {
+	public function sendEmail($title, $html,$email)
+	{
 		
-	// 	require_once "./app/libraries/PHPMailer/src/PHPMailer.php";
-	// 	require_once "./app/libraries/PHPMailer/src/SMTP.php";
-	// 	require_once "./app/libraries/PHPMailer/src/Exception.php";
-	// 	$mail = new PHPMailer(true);
-	// 	$mail->isSMTP();
-	// 	$mail->Host = 'smtp.gmail.com';
-	// 	$mail->SMTPAuth = true;
-	// 	$mail->Username = "hoangnlpd09770@fpt.edu.vn";
-	// 	$mail->Password = "tttv fmok txsh ivae";
-	// 	$mail->SMTPSecure = 'ssl';
-	// 	$mail->Port = 465;
-	// 	$mail->addAddress($email);
-	// 	$mail->CharSet = 'UTF-8';
-	// 	$mail->isHTML(true);
-	// 	$mail->Subject = $title;
-	// 	$mail->Body = $html;
-	// 	$mail->send();
-	// }
+		require_once "./app/libraries/PHPMailer/src/PHPMailer.php";
+		require_once "./app/libraries/PHPMailer/src/SMTP.php";
+		require_once "./app/libraries/PHPMailer/src/Exception.php";
+		$mail = new PHPMailer(true);
+		$mail->isSMTP();
+		$mail->Host = 'smtp.gmail.com';
+		$mail->SMTPAuth = true;
+		$mail->Username = "hoangnlpd09770@fpt.edu.vn";
+		$mail->Password = "tttv fmok txsh ivae";
+		$mail->SMTPSecure = 'ssl';
+		$mail->Port = 465;
+		$mail->addAddress($email);
+		$mail->CharSet = 'UTF-8';
+		$mail->isHTML(true);
+		$mail->Subject = $title;
+		$mail->Body = $html;
+		$mail->send();
+	}
 }
